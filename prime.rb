cf773eb62fcde9d6605a3d02e5ee32ded1c4853d
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-02 14:00:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 # Add  code here!
 
 def prime?(number)
-  divisors = (2..(number - 1)).to_a
-  # divisors = [2, 3, 4, ... , number - 1]
   if number <= 1
     false
   elsif number == 2
@@ -10,6 +8,7 @@ def prime?(number)
   elsif number == 3
     true
   else
+    divisors = (2..(number - 1)).to_a
     divisors.each do |divisor|
       if number % divisor == 0
         false
