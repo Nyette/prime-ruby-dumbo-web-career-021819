fba3c3a688d7f36fbf38f6568949cf206db88de9
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-02 13:39:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,7 @@
 # Add  code here!
 
 def prime?(number)
-  range = []
-  for divisor in 2..(number - 1)
-    range << divisor
-  end
-  if number <= 1
-    return false
-  else
-    range.each do |divisor|
-      if (number % divisor == 0)
-        return false
-      else
-        return true
-      end
-    end
-  end
+  divisors = (2..(number - 1))
+  
 end
 
