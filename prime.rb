bbd878d40ebc5b977c6186d937f093dd4785e2ea
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-02 13:35:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime?(number)
+  range = []
+  for divisor in 2..(number - 1)
+    range << divisor
+  end
+  if number <= 1
+    return false
+  else
+    range.each do |divisor|
+      if (number % divisor == 0)
+        return false
+      else
+        return true
+      end
+    end
+  end
+end
+
