f3798b720516715424041a2b3aae9e3aea7af0c3
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-02 13:55:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,10 +5,14 @@ def prime?(number)
   # divisors = [2, 3, 4, ... , number - 1]
   if number <= 1
     false
+  elsif number == 2
+    true
+  elsif number == 3
+    true
   else
     divisors.each do |divisor|
-      if number %   
-  
+      if number % divisor == 0
+        false
       end
     end
   end
